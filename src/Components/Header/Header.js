3fdc9ba8426fdb3bb19270473c3d0a24c7363df7
MachineKe <markarapsoi72@gmail.com>
2024-05-22 18:31:50 +0300

    romeve comments

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,4 @@
 import React from "react";
-import {
-  FaEnvelope,
-  FaPhone,
-  FaFacebookF,
-  FaTwitter,
-  FaInstagram,
-  FaPinterest,
-} from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -16,32 +8,6 @@ const Header = () => {
         alt="logo"
         className="w-[150px] mb-4 md:mb-0"
       />
-      {/* 
-      <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
-        <div className="flex items-center space-x-2">
-          <FaEnvelope className="text-blue-600" />
-          <a href="mailto:detective@daviskicapital.com" className="text-blue-600">detective@daviskicapital.com</a>
-        </div>
-        <div className="flex items-center space-x-2">
-          <FaPhone className="text-blue-600" />
-          <a href="tel:+254797713396" className="text-blue-600">+254797713396</a>
-        </div>
-      </div>
-
-      <div className="flex space-x-4 mt-4 md:mt-0">
-        <a href="https://www.facebook.com" className="text-blue-600 hover:text-blue-800">
-          <FaFacebookF className="w-6 h-6" />
-        </a>
-        <a href="https://www.twitter.com" className="text-blue-600 hover:text-blue-800">
-          <FaTwitter className="w-6 h-6" />
-        </a>
-        <a href="https://www.instagram.com" className="text-blue-600 hover:text-blue-800">
-          <FaInstagram className="w-6 h-6" />
-        </a>
-        <a href="https://www.pinterest.com" className="text-blue-600 hover:text-blue-800">
-          <FaPinterest className="w-6 h-6" />
-        </a>
-      </div> */}
     </div>
   );
 };
