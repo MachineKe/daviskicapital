61145fb145485c842eada419883e00d17b0a851f
MachineKe <markarapsoi72@gmail.com>
2024-05-22 16:59:45 +0300

    make mobile responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,22 +3,25 @@ import { FaEnvelope, FaPhone, FaFacebookF, FaTwitter, FaInstagram, FaPinterest }
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-4">
-            <img
+    <div className="flex flex-col md:flex-row justify-between items-center p-4">
+      <img
         src="https://res.cloudinary.com/dmpposta9/image/upload/v1716289787/beyond/daviskicapital/The_Data_Detectiv_removeb_preview_whk6dc.png"
         alt="logo"
-        className="w-[150px]"
+        className="w-[150px] mb-4 md:mb-0"
       />
+{/* 
+      <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
+        <div className="flex items-center space-x-2">
+          <FaEnvelope className="text-blue-600" />
+          <a href="mailto:detective@daviskicapital.com" className="text-blue-600">detective@daviskicapital.com</a>
+        </div>
+        <div className="flex items-center space-x-2">
+          <FaPhone className="text-blue-600" />
+          <a href="tel:+254797713396" className="text-blue-600">+254797713396</a>
+        </div>
+      </div>
 
-      <div className="flex items-center space-x-2">
-        <FaEnvelope className="text-blue-600" />
-        <a href="mailto:detective@daviskicapital.com" className="text-blue-600">detective@daviskicapital.com</a>
-      </div>
-      <div className="flex items-center space-x-2">
-        <FaPhone className="text-blue-600" />
-        <a href="tel:+254797713396" className="text-blue-600">+254797713396</a>
-      </div>
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 mt-4 md:mt-0">
         <a href="https://www.facebook.com" className="text-blue-600 hover:text-blue-800">
           <FaFacebookF className="w-6 h-6" />
         </a>
@@ -31,7 +34,7 @@ const Header = () => {
         <a href="https://www.pinterest.com" className="text-blue-600 hover:text-blue-800">
           <FaPinterest className="w-6 h-6" />
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Nav.css'; // Import CSS file for styling
 import { FaHome } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { FaBriefcaseMedical } from "react-icons/fa6";
 import { FaTasks } from "react-icons/fa";
 
-
 const Nav = () => {
   return (
-    <nav className="navbar">
-      <Link to="/regApp" className="nav-item"><FaHome /></Link>
-      <Link to="" className="nav-item"><FaBriefcaseMedical /></Link>
-      <Link to="" className="nav-item"><FaTasks/></Link>
-      <Link to="/" className="nav-item"><CgProfile /></Link>
-      
+    <nav className="fixed bottom-0 w-full bg-gray-800 p-4 flex justify-around md:justify-center md:space-x-8 text-white">
+      <Link to="/regApp" className="nav-item text-2xl md:text-xl hover:text-blue-400"><FaHome /></Link>
+      <Link to="#" className="nav-item text-2xl md:text-xl hover:text-blue-400"><FaBriefcaseMedical /></Link>
+      <Link to="#" className="nav-item text-2xl md:text-xl hover:text-blue-400"><FaTasks /></Link>
+      <Link to="/" className="nav-item text-2xl md:text-xl hover:text-blue-400"><CgProfile /></Link>
     </nav>
   );
 }
